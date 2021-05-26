e2d434fb26c085d0b0f0a3192db3b3e7ebcac2f3
Jolita <>
2021-05-26 06:05:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//manipalation DOM
+//first i have to create variables
+const numberOfRows = document.getElementById('rows');
+//please crete variable for columns
+
+const button= document.querySelector('button')
+button.addEventListener("click", createTable);
+
+function createTable(){
+  //how many lines? now you can get the values from numberOfRows
+
+  console.log("Labas")
+}
